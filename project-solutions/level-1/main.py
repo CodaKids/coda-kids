15845bfdbc119a169d3509d40beef4b743cf5cb6
Kieran Williams <kieranw.gamedev@gmail.com>
2018-08-20 15:16:13 -0700

    Added art and usable turtle instructions to level 1.

--- project-solutions/level-1/main.py
+++ project-solutions/level-1/main.py
@@ -4,22 +4,32 @@ import turtle
 # Create our screen
 SCREEN = turtle.Screen()
 SCREEN.title("Napster Spacebook")
-SCREEN.setup(500, 500)
+SCREEN.setup(1280, 800)
 SCREEN.bgpic("Background.png")
+
+# Code is explained by the book
 turtle.penup()
-#turtle.setx(-230)
-#turtle.sety(-230)
+turtle.setx(460)
+turtle.sety(-275)
 turtle.pendown()
+
+# Student uncomments
+turtle.left(180)
+turtle.forward(510)
+
+# Student copies from the book
+turtle.right(90)
+turtle.forward(220)
+
+# Student follows instructions from the book
+turtle.left(90)
+turtle.forward(490)
+turtle.right(90)
+turtle.forward(250)
+
+# Happens every frame.
+# Loop no movement so that the program doesn't close at the end.
+# There's probably a better way to do this, but is it simpler/more understandable for students?
 RUNNING = True
-
-#Happens once.
-#turtle.right(90)
-#turtle.forward(100)
-#turtle.left(45)
-#turtle.forward(100)
-
-#Happens every frame.
 while RUNNING:
-    turtle.forward(2)
-    turtle.right(2)
-
+    turtle.left(0)
