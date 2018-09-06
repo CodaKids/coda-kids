681c75a61a03814814a3929fa2abde4f38714fb6
Kieran Williams <kieranw.gamedev@gmail.com>
2018-09-06 11:14:38 -0700

    Added easy/medium/hard solutions to level 1 code.

--- project-solutions/level-1/main.py
+++ project-solutions/level-1/main.py
@@ -16,18 +16,83 @@ turtle.setx(460)
 turtle.sety(-275)
 turtle.pendown()
 
-# Student uncomments
-turtle.left(180)
-turtle.forward(510)
+# Easy
+if True:
+    # Student uncomments
+    turtle.left(180)
+    turtle.forward(510)
 
-# Student copies from the book
-turtle.right(90)
-turtle.forward(220)
+    # Student copies from the book
+    turtle.right(90)
+    turtle.forward(220)
 
-# Student follows instructions from the book
-turtle.left(90)
-turtle.forward(490)
-turtle.right(90)
-turtle.forward(250)
+    # Student follows instructions from the book
+    turtle.left(90)
+    turtle.forward(490)
+    turtle.right(90)
+    turtle.forward(250)
+
+#Medium
+if False:
+    turtle.left(180)
+    turtle.forward(125)
+
+    turtle.right(90)
+    turtle.forward(105)
+
+    turtle.right(90)
+    turtle.forward(185)
+
+    turtle.left(90)
+    turtle.forward(385)
+
+    turtle.left(90)
+    turtle.forward(65)
+
+    turtle.right(90)
+    turtle.forward(105)
+
+    turtle.left(90)
+    turtle.forward(435)
+
+    turtle.left(90)
+    turtle.forward(105)
+
+    turtle.right(90)
+    turtle.forward(195)
+
+    turtle.right(90)
+    turtle.forward(80)
+
+    turtle.left(90)
+    turtle.forward(170)
+
+#Hard
+if False:
+    for i in range(5):
+        turtle.left(180)
+        turtle.forward(510)
+
+        turtle.right(90)
+        turtle.forward(220)
+
+        turtle.left(90)
+        turtle.forward(490)
+        turtle.right(90)
+        turtle.forward(250)
+
+        # Reached the school
+
+        turtle.left(180)
+        turtle.forward(250)
+        turtle.left(90)
+        turtle.forward(490)
+        turtle.right(90)
+
+        turtle.forward(220)
+        turtle.left(90)
+
+        turtle.forward(510)
+
 
 turtle.done()
