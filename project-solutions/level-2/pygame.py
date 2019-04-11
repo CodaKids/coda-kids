8da832ee7233110cb80a156da1ca22241e1666f9
Hannah Grandine <hannahgrandine@gmail.com>
2019-04-11 15:31:42 -0700

    Updated to reflect naming conventions

--- project-solutions/level-2/pygame.py
+++ project-solutions/level-2/pygame.py
@@ -1,149 +1,144 @@
-import pygame, sys
-import time
+import pygame
 from os import path
 
-def getFile(fileName):
+def get_file(fileName):
     """Returns the absolute path of a file."""
+    #This grabs your files from your folder.
     return path.join(path.dirname(__file__), fileName)
 
 """Loads the background and images"""
-BACKGROUND = pygame.image.load(getFile('assets/ClassroomBackground.png'))
-CHARACTER_1 = pygame.image.load(getFile('assets/Character_1.png'))
-ANN = pygame.image.load(getFile("assets/Ann.png"))
-BEYO = pygame.image.load(getFile("assets/Beyo.png"))
-GRAPHICA = pygame.image.load(getFile("assets/Graphica.png"))
-IAN_FIB = pygame.image.load(getFile("assets/IanFib.png"))
-INTELL = pygame.image.load(getFile("assets/Intell.png"))
-JAVA_LIN = pygame.image.load(getFile("assets/JavaLin.png"))
-JAVO = pygame.image.load(getFile("assets/Javo.png"))
-JITTER = pygame.image.load(getFile("assets/Jitter.png"))
-PAUL_PYTHON = pygame.image.load(getFile("assets/PaulPython.png"))
-QUACK = pygame.image.load(getFile("assets/Quack.png"))
-RAM_ROM = pygame.image.load(getFile("assets/Ram_Rom.png"))
-SBT = pygame.image.load(getFile("assets/SBT.png"))
-SIDE_WINDER = pygame.image.load(getFile("assets/SideWinderNEw.png"))
-SYNTAX = pygame.image.load(getFile("assets/Syntax.png"))
+background = pygame.image.load(get_file('assets/ClassroomBackground.png'))
+ann = pygame.image.load(get_file("assets/Ann.png"))
+beyo = pygame.image.load(get_file("assets/Beyo.png"))
+graphica = pygame.image.load(get_file("assets/Graphica.png"))
+ian_fib = pygame.image.load(get_file("assets/IanFib.png"))
+intell = pygame.image.load(get_file("assets/Intell.png"))
+java_lin = pygame.image.load(get_file("assets/JavaLin.png"))
+javo = pygame.image.load(get_file("assets/Javo.png"))
+jitter = pygame.image.load(get_file("assets/Jitter.png"))
+paul_python = pygame.image.load(get_file("assets/PaulPython.png"))
+quack = pygame.image.load(get_file("assets/Quack.png"))
+ram_rom = pygame.image.load(get_file("assets/Ram_Rom.png"))
+sbt = pygame.image.load(get_file("assets/SBT.png"))
+side_winder = pygame.image.load(get_file("assets/SideWinder.png"))
+syntax = pygame.image.load(get_file("assets/Syntax.png"))
 
 """Stores character text into variables"""
-TEXT_1  = "I am character 1!"
-TEXT_2  = "I am character 2!"
-TEXT_3  = "I am character 3!"
-TEXT_4  = "I am character 4!"
-TEXT_5  = "I am character 5!"
-TEXT_6  = "I am character 6!"
-TEXT_7  = "I am character 7!"
-TEXT_8  = "I am character 8!"
-TEXT_9  = "I am character 9!"
-TEXT_10 = "I am character 10!"
-TEXT_11 = "I am character 11!"
-TEXT_12 = "I am character 12!"
-TEXT_13 = "I am character 13!"
-TEXT_14 = "I am character 14!"
-TEXT_15 = "I am character 15!"
-
+text_ann  = "I am character 1!"
+text_beyo  = "I am character 2!"
+text_graphica  = "I am character 3!"
+text_ian_fib  = "I am character 4!"
+text_intell  = "I am character 5!"
+text_java_lin  = "I am character 6!"
+text_javo  = "I am character 7!"
+text_jitter  = "I am character 8!"
+text_paul_python  = "I am character 9!"
+text_quack = "I am character 10!"
+text_ram_rom = "I am character 11!"
+text_sbt = "I am character 12!"
+text_side_winder = "I am character 13!"
+text_syntax = "I am character 14!"
 
 """Initialize Font Object"""
 pygame.init()
 myfont = pygame.font.SysFont(None, 35)
 
 """We assign our character and text variables"""
-character = CHARACTER_1
-text = TEXT_1
+current_character = ann
+current_text = text_ann
 
 """Displays character to the screen"""
-WIDTH = 638
-HEIGHT = 825
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+width = 638
+height = 825
+screen = pygame.display.set_mode((width,height))
 running = True
 
 # """solution using if-statements"""
 # while running:
-#     SCREEN.blit(BACKGROUND,(0,0))
-#     SCREEN.blit(character, (0,0))
-#     SCREEN.blit(myfont.render(text, True, (255, 0, 0)), (235,250))
+#     screen.blit(background,(0,0))
+#     screen.blit(current_character, (0,0))
+#     screen.blit(myfont.render(current_text, True, (255, 0, 0)), (235,250))
 #     pygame.display.flip()
 #     events = pygame.event.get()
 #     for event in events:
 #         if event.type == pygame.KEYDOWN:
 #             if event.key == pygame.K_1:
-#                 character = CHARACTER_1
-#                 text = TEXT_1 
+#                 character = ann
+#                 text = text_ann
 #             if event.key == pygame.K_2:
-#                 character = ANN
-#                 text = TEXT_2 
+#                 character = beyo
+#                 text = text_beyo 
 #             if event.key == pygame.K_3:
-#                 character = ANN
-#                 text = TEXT_3 
+#                 character = graphica
+#                 text = text_graphica 
 #             if event.key == pygame.K_4:
-#                 character = ANN
-#                 text = TEXT_4 
+#                 character = ian_fib
+#                 text = text_ian_fib 
 #             if event.key == pygame.K_5:
-#                 character = ANN
-#                 text = TEXT_5
+#                 character = intell
+#                 text = text_intell
 #             if event.key == pygame.K_6:
-#                 character = ANN
-#                 text = TEXT_6
+#                 character = java_lin
+#                 text = text_java_lin
 #             if event.key == pygame.K_7:
-#                 character = ANN
-#                 text = TEXT_7
+#                 character = javo
+#                 text = text_javo
 #             if event.key == pygame.K_8:
-#                 character = ANN
-#                 text = TEXT_8
+#                 character = jitter
+#                 text = text_jitter
 #             if event.key == pygame.K_9:
-#                 character = ANN
-#                 text = TEXT_9
+#                 character = paul_python
+#                 text = text_paul_python
 #             if event.key == pygame.K_0:
-#                 character = ANN
-#                 text = TEXT_10
+#                 character = quack
+#                 text = text_quack
 #             if event.key == pygame.K_q:
 #                 running = False
 # pygame.quit()
 
 """solution using an array"""
 #initialize arrays
-characters = []
-texts = []
+CHARACTERS = []
+TEXT = []
 #add characters and text to arrays
-characters.append(CHARACTER_1)
-texts.append(TEXT_1)
-characters.append(ANN)
-texts.append(TEXT_2)
-characters.append(BEYO)
-texts.append(TEXT_3)
-characters.append(GRAPHICA)
-texts.append(TEXT_4)
-characters.append(IAN_FIB)
-texts.append(TEXT_5)
-characters.append(INTELL)
-texts.append(TEXT_6)
-characters.append(JAVA_LIN)
-texts.append(TEXT_7)
-characters.append(JAVO)
-texts.append(TEXT_8)
-characters.append(JITTER)
-texts.append(TEXT_9)
-characters.append(PAUL_PYTHON)
-texts.append(TEXT_10)
-characters.append(QUACK)
-texts.append(TEXT_11)
-characters.append(RAM_ROM)
-texts.append(TEXT_12)
-characters.append(SBT)
-texts.append(TEXT_13)
-characters.append(SIDE_WINDER)
-texts.append(TEXT_14)
-characters.append(SYNTAX)
-texts.append(TEXT_15)
+CHARACTERS.append(ann)
+TEXT.append(text_ann)
+CHARACTERS.append(beyo)
+TEXT.append(text_beyo)
+CHARACTERS.append(graphica)
+TEXT.append(text_graphica)
+CHARACTERS.append(ian_fib)
+TEXT.append(text_ian_fib)
+CHARACTERS.append(intell)
+TEXT.append(text_intell)
+CHARACTERS.append(java_lin)
+TEXT.append(text_java_lin)
+CHARACTERS.append(javo)
+TEXT.append(text_javo)
+CHARACTERS.append(jitter)
+TEXT.append(text_jitter)
+CHARACTERS.append(paul_python)
+TEXT.append(text_paul_python)
+CHARACTERS.append(quack)
+TEXT.append(text_quack)
+CHARACTERS.append(ram_rom)
+TEXT.append(text_ram_rom)
+CHARACTERS.append(sbt)
+TEXT.append(text_sbt)
+CHARACTERS.append(side_winder)
+TEXT.append(text_side_winder)
+CHARACTERS.append(syntax)
+TEXT.append(text_syntax)
 
 running = True
 #start at the first character in the array
 i = 0
 while running:
-    character = characters[i]
-    text = texts[i]
-    SCREEN.blit(BACKGROUND,(0,0))
-    SCREEN.blit(character, (0,0))
-    SCREEN.blit(myfont.render(text, True, (255, 0, 0)), (235,250))
+    current_character = CHARACTERS[i]
+    current_text = TEXT[i]
+    screen.blit(background,(0,0))
+    screen.blit(current_character, (0,0))
+    screen.blit(myfont.render(current_text, True, (255, 0, 0)), (235,250))
     pygame.display.flip()
     events = pygame.event.get()
     for event in events:
@@ -155,6 +150,6 @@ while running:
             if event.key == pygame.K_q:
                 running = False
     #if we reach the end of the array, start from the beginning again.
-    if i == 15:
+    if i == 1:
         i = 0
 pygame.quit()
