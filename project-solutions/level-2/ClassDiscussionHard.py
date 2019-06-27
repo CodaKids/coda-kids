e53fe2695a44c242a9c7e10dedf92f58316dd359
Hannah Grandine <hannahgrandine@gmail.com>
2019-06-26 21:49:16 -0500

    added display_text function to implement text wrap

--- project-solutions/level-2/ClassDiscussionHard.py
+++ project-solutions/level-2/ClassDiscussionHard.py
@@ -1,5 +1,6 @@
 import pygame #Gives us our gaming functions
 from os import path
+import textwrap
 
 """Initialize Font Object"""
 #We pick our text style and size.
@@ -10,6 +11,13 @@ def get_file(fileName):
     """Returns the absolute path of a file."""
     #This grabs your files from your folder.
     return path.join(path.dirname(__file__), fileName)
+
+def display_text(screen, current_text):
+    wrapped_text = textwrap.wrap(current_text, 30)
+    y = 230
+    for i in range(len(wrapped_text)):
+        screen.blit(myfont.render(wrapped_text[i], True, (0, 0, 0)), (230,y))
+        y = y + 30
 
 """Loads the background and images"""
 background = pygame.image.load(get_file("assets/Background.png"))
@@ -90,7 +98,7 @@ while running:
     current_text = TEXT[i]
     screen.blit(background,(0,0))
     screen.blit(current_character, (0,0))
-    screen.blit(myfont.render(current_text, True, (0, 0, 0)), (235,250))
+    display_text(screen, current_text)
     pygame.display.flip()
     events = pygame.event.get()
     for event in events:
