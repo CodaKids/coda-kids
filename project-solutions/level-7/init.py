f226802b811fe7af89de1ea298b37b4d43b257bf
ozabell <kidlatmc@gmail.com>
2020-09-26 08:50:42 -0700

    added start and stop fxn, starting on Machine class

--- project-solutions/level-7/init.py
+++ project-solutions/level-7/init.py
@@ -19,8 +19,29 @@ _data = {}
 
 #============================================================
 #PART 2: CREATING A FRAMEWORK OF GENERAL CLASSES AND FUNCTIONS
+def start(window_size, game_name):
+    """ Initializes the library and returns a pygame screen.  """
+    pygame.init()
+    pygame.display.set_caption(game_name)
+    pygame.mixer.init()
+    return pygame.display.set_mode((int(window_size[0]), int(window_size[1])))
 
+def stop():
+    """ Stops pygame and closes the window immediately. """
+    sys.exit()
 
+class Machine:
+    """Game state machine class."""
+     def __init__(self):
+        self.current = 0
+        self.previous = 0
+        self.states = []
 #============================================================
 #PART 3: SETUP FOR THE BATTLE CARDS GAME
+Manager = Machine()
 
+#constants for screen
+WINDOW_WIDTH = 800
+WINDOW_LENGTH = 600
+WINDOW = pygame.math.Vector2(WINDOW_WIDTH, WINDOW_LENGTH)
+SCREEN = start(WINDOW, "IncrediCards")
