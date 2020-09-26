150104d534e78815ce940deacbe9ba4e4829376f
ozabell <kidlatmc@gmail.com>
2020-09-26 09:00:04 -0700

    added run fxn

--- project-solutions/level-7/init.py
+++ project-solutions/level-7/init.py
@@ -36,6 +36,34 @@ class Machine:
         self.current = 0
         self.previous = 0
         self.states = []
+
+    def register(self, module):
+        """Registers the state's init, update, draw, and cleanup functions."""
+        self.states.append({'initialize': module.initialize,
+                            'update': module.update,
+                            'draw': module.draw,
+                            'cleanup': module.cleanup})
+
+    # unsure if update fxn it needed
+
+    def run(self, screen, window, fill_color):
+        """Runs the state given machine."""
+        clock = pygame.time.Clock()
+        # first run initialize!
+        self.states[self.current]['initialize'](window)
+
+        while True:
+            delta_time = clock.tick(60) / 1000
+            if self.current != self.previous:
+                self.states[self.current]['cleanup']()
+                self.states[self.current]['initialize'](window)
+                self.previous = self.current
+
+            update(delta_time)
+            self.states[self.current]['update'](delta_time)
+            screen.fill(fill_color)
+            self.states[self.current]['draw'](screen)
+            pygame.display.flip()
 #============================================================
 #PART 3: SETUP FOR THE BATTLE CARDS GAME
 Manager = Machine()
